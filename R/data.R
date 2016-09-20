23069a0af76957d52523a9a0343abbe6a1c7db7d
Jesse Piburn <jesse.piburn@gmail.com>
2016-09-20 15:54:48 -0400

    update version number and cache

--- R/data.R
+++ R/data.R
@@ -4,7 +4,7 @@
 #' By default functions \code{\link{wb}} and \code{\link{wbsearch}} use this
 #' data for the \code{cache} parameter.
 #'
-#' This data was updated using \code{\link{wbcache}} on May 19, 2016
+#' This data was updated using \code{\link{wbcache}} on September 20, 2016
 #'
 #' @format A list containing 7 data frames:
 #' \itemize{
